81b79aa18518b1723e90ae0859191b5093017487
Solomon Olatunji <iamsolomonolatunji@gmail.com>
2026-07-12 05:29:55 +0100

    chore: setup test directories for main Go backend

---
+++ tests/integration/backend_test.go
@@ -0,0 +1,10 @@
+package integration_test
+
+import (
+	"testing"
+)
+
+func TestVesslBackendInitialization(t *testing.T) {
+	// TODO: Initialize the main vessl backend services and assert they start correctly
+	t.Skip("Pending integration test setup for vessl backend")
+}
